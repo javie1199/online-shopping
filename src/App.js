c756928fd95a52c2cbc983d360fb744ff7c62ca2
Jon Tran <trungt8998@gmail.com>
2021-11-15 08:48:17 -0600

    Add Sign-In-Sign-Up Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import ShopPage from "./pages/shoppage/shoppage.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp}></Route>
       </Switch>
     </div>
   );
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+import "./sign-in.styles.scss";
+import "../form-input/form-input.component";
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+class SignIn extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = (event) => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value }); //dynamically key value, the name could be email/password
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+        <FormInput
+          type="email"
+          name="email"
+          required
+          value={this.state.email}
+          label="email"
+          onChange={this.handleChange}
+        />
+        <FormInput
+          type="password"
+          name="password"
+          required
+          value={this.state.password}
+          label="password"
+          onChange={this.handleChange}
+        />
+        <CustomButton>Sign In</CustomButton>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
