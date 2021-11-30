6f075029696a3789782bb40be16547596a8e0d85
Jon Tran <trungt8998@gmail.com>
2021-11-29 22:33:21 -0600

    add comments for explaination of Redux

---
+++ src/redux/shop/shop.action.js
@@ -0,0 +1,1 @@
+// define what kind of events could happen in the app
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -2,11 +2,13 @@ const INIITAL_STATE = {
   currentUser: null,
 };
 
+// if the no state pass out to userReducer, initial state will be used.
 const userReducer = (state = INIITAL_STATE, action) => {
   switch (action.type) {
     case "SET_CURRENT_USER":
+      // reducer needs to return a new object so that react component will be rerendered
       return {
-        ...state,
+        ...state, // get other properties of current state
         currentUser: action.payload,
       };
 
